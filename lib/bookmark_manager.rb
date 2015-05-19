7f1ab23fea84e95008210c8685c8af995481012e
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-05-19 17:03:00 +0100

    completed stage 1 tags

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'data_mapper'
 require './lib/link'
+require 'tag'
 
 
 env = ENV[ 'RACK_ENV'] || 'development'
@@ -10,9 +11,28 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
-    'Hello BookmarkManager!'
+    @links = Link.all
+    erb :index
   end
+
+  post '/links' do
+    url = params['url']
+    title = params['title']
+    tags = params['tags'].split(' ').map do |tag|
+      Tag.first_or_create(text: tag)
+    end
+    Link.create(url: url, title: title, tags: tags)
+    redirect to('/')
+  end
+
+  get '/tags/:text' do
+    tag = Tag.first(text: params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
