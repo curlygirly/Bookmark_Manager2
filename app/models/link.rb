53a18f46bc4ba56f41e50083d937a1bdff0abf76
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-05-20 15:54:44 +0100

    Tweak to model link rb

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,11 +2,14 @@ class Link
 
   include DataMapper::Resource
  # this makes the instances of this class Datamapper resources
+
+  has n, :tags, through: Resource
+
   property :id, Serial
   property :title, String
   property :url, String
 
-  has n, :tags, through: Resource
+
 
 
 end
